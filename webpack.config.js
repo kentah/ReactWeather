0b6bf507e65101f7ecf72030d0bf5c0a0096c766
kentah <kenthow@gmail.com>
2016-11-30 20:07:25 -0600

    Swtich to SCSS

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
       WeatherMessage: 'app/components/WeatherMessage.jsx',
       openWeathermap: 'app/api/openWeathermap.jsx',
       ErrorModal: 'app/components/ErrorModal.jsx',
-      applicationStyles: 'app/styles/app.css'
+      applicationStyles: 'app/styles/app.scss'
     },
     extensions: ['', '.js', '.jsx']
   },
